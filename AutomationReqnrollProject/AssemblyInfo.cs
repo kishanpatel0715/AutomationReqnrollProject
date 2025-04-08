4df77cce2fbf2a9afba693f9002805d0430fe43c
Kishan Patel <kishanpatel0715@gmail.com>
2025-04-08 17:23:54 -0600

    Added code to run TCs parallely

---
+++ AutomationReqnrollProject/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+[assembly: Parallelizable(ParallelScope.Fixtures)]
+[assembly: LevelOfParallelism(3)]
+
+namespace AutomationReqnrollProject
+{
+    public class AssemblyInfo
+    {
+      
+    }
+}
